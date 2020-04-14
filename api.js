6a8429b38deca8f3fe005269e428251066664aa6
Brendan Lam <Brendan@Brendans-MacBook-Pro-3.local>
2020-04-14 10:56:20 -0700

    Fixed json object

--- api.js
+++ api.js
@@ -32,26 +32,28 @@ function generate_country_object( country_data ) {
     let deaths = country_data.latest_data.deaths;
     let country_info = {
         "seriesname": country_name,
-        "data": [
+        "latest_data": [
             { "confirmed": confirmed },
             { "recovered": recovered },    
             { "deaths": deaths },
         ]     
-    }
+    };
     return country_info;
 }
 
 function find_top_five_countries_by_confirmed_cases( country_data ) {
-    let object = [];
+    let object = { 
+        "data": []
+    };
     for( let country of country_data) {
         let confirmed = country.latest_data.confirmed;
-        if( object.length < total_countries_tracked ) {
-            object.push(generate_country_object(country));
+        if( object.data.length < total_countries_tracked ) {
+            object.data.push(generate_country_object(country));
         }
         else {
-            let index_of_smallest_confirmed_country_from_set = find_index_of_smallest_confirmed_case( object );
-            if( confirmed > object[index_of_smallest_confirmed_country_from_set].data[0].confirmed ) {
-                object[index_of_smallest_confirmed_country_from_set] = generate_country_object(country);
+            let index_of_smallest_confirmed_country_from_set = find_index_of_smallest_confirmed_case( object.data );
+            if( confirmed > object.data[index_of_smallest_confirmed_country_from_set].latest_data[0].confirmed ) {
+                object.data[index_of_smallest_confirmed_country_from_set] = generate_country_object(country);
             }
         }
     }
@@ -61,7 +63,7 @@ function find_top_five_countries_by_confirmed_cases( country_data ) {
 function find_index_of_smallest_confirmed_case( top_five_countries ) {
     let current_smallest = top_five_countries[0];
     for( let country of top_five_countries ) {
-        if( country.data[0].confirmed < current_smallest.data[0].confirmed ) {
+        if( country.latest_data[0].confirmed < current_smallest.latest_data[0].confirmed ) {
             current_smallest = country;
         }
     }
